87c713d440ad1be507a2c016bf17356503e434a5
anton <anton.larsen@persequor.com>
2021-11-22 04:32:48 +0100

    Fixed broken generic type

--- core/src/main/java/io/ran/CrudRepositoryBaseRepo.java
+++ core/src/main/java/io/ran/CrudRepositoryBaseRepo.java
@@ -2,7 +2,7 @@ package io.ran;
 
 public interface CrudRepositoryBaseRepo<T, K, Q extends CrudRepository.InlineQuery<T,Q>> extends CrudRepository<T, K> {
 	Q query();
-	default <O, QO extends CrudRepository.InlineQuery<Q, QO>> QO query(Class<O> oClass) {
+	default <O, QO extends CrudRepository.InlineQuery<O, QO>> QO query(Class<O> oClass) {
 		throw new RuntimeException("Querying other models is not yet supported by this crud repository");
 	}
 }
