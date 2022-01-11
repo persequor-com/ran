ec9bbb49ba3320b89f3cce4e5828757b198d2beb
anton <anton.larsen@persequor.com>
2022-01-11 07:50:30 +0100

    OPEN-12 added convenience method for adding indexes using KeySet classes, also to the index describing this

--- core/src/main/java/io/ran/schema/ITableBuilder.java
+++ core/src/main/java/io/ran/schema/ITableBuilder.java
@@ -1,14 +1,19 @@
 package io.ran.schema;
 
+import io.ran.KeySet;
+import io.ran.Property;
 import io.ran.token.Token;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
 
 public interface ITableBuilder<TB extends ITableBuilder<TB, CB, IB>, CB extends ColumnBuilder<CB>, IB extends IndexBuilder<IB>> {
+	TB addColumn(Property property);
 	TB addColumn(Token token, Class type);
 	TB addColumn(Token token, Class type, Consumer<CB> consumer);
+	TB addPrimaryKey(KeySet key);
 	TB addPrimaryKey(Token... id);
 	TB addIndex(Token name, Token... id);
 	TB addIndex(Token name, Consumer<IB> consumer);
+	TB addIndex(KeySet key);
 }
