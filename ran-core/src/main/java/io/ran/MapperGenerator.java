0fb110fc86b6731ae82cccfaa19b2bd7caf2fe64
andre-psqr <andre.vigneaux@psqr.eu>
2022-05-12 15:58:41 +0200

    remove debug stuff

--- ran-core/src/main/java/io/ran/MapperGenerator.java
+++ ran-core/src/main/java/io/ran/MapperGenerator.java
@@ -12,18 +12,18 @@ public class MapperGenerator {
 	public Wrapped generate(AutoMapperClassLoader classLoader, Clazz clazz) {
 		try {
 			Path path = Paths.get("/tmp/ran/"+clazz.getSimpleName()+"Mapper.class");
-			Path pathQuery = Paths.get("/tmp/ran/"+clazz.getSimpleName()+"Query.class");
+//			Path pathQuery = Paths.get("/tmp/ran/"+clazz.getSimpleName()+"Query.class");
 
 			MappingClassWriter visitor = new MappingClassWriter(clazz.clazz);
 			byte[] bytes = visitor.toByteArray();
-			Files.write(path, bytes);
+//			Files.write(path, bytes);
 			CheckClassAdapter.verify(new ClassReader(bytes),false, new PrintWriter(System.out));
 
 
 			QueryClassWriter visitor2 = new QueryClassWriter(clazz.clazz);
 			byte[] bytes2 = visitor2.toByteArray();
 			CheckClassAdapter.verify(new ClassReader(bytes2),false, new PrintWriter(System.out));
-			Files.write(pathQuery, bytes2);
+//			Files.write(pathQuery, bytes2);
 
 
 			return new Wrapped(classLoader.define(visitor.getName(), bytes), classLoader.define(visitor2.getName(), bytes2));
